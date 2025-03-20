a185e68621d64ee2feb9bba390283278cac32fa5
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-03-20 15:59:07 +0200

    update

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -209,7 +209,7 @@ if not are_installed_1() then install_profiles(1) end
 local url = 'https://github.com/VapeVoidware/VWPacket'
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = 'cc48be80dc94853846795bff362bccae0490d792'
+commit = '6b23585308921c6266fa3c9af561df7a4bd0839a'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
