32afa9c2378361540cee9701214c6c3d4c10a40e
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-04-12 11:49:11 +0300

    Images in UpdateLog + Packet Returns! 🌟
    + Added images to the UpdateLog
    + VW PACKET IS BACK!
    [VW REWRITE, PACKET]
    + Optimised the nuker A LOT
    + Fixed Scaffold [WoolOnly] check
    [VW OLD, RISE]
    + Added block breaking gui from VW Rewrite
    [VW REWRITE, PACKET, OLD, RISE]
    + Optimised AutoWhisper - Thanks groya9633#0
    
    + Added /eggclaim chat command to claim the Cheatcode Egg!

--- games/VW6872274481.lua
+++ games/VW6872274481.lua
@@ -1,7 +1,8 @@
+local lplr = game:GetService("Players").LocalPlayer
+
 task.spawn(function()
     pcall(function()
         repeat task.wait() until shared.vapewhitelist
-        local lplr = game:GetService("Players").LocalPlayer
         local char = lplr.Character or lplr.CharacterAdded:wait()
         local displayName = char:WaitForChild("Head"):WaitForChild("Nametag"):WaitForChild("DisplayNameContainer"):WaitForChild("DisplayName")
         repeat task.wait() until shared.vapewhitelist
@@ -17,6 +18,26 @@ task.spawn(function()
             end
         end)
     end)
+end)
+
+pcall(function()
+	local cheat = {57,84,142,96,195,198,254,218,104,79,208,20,197,34,10,112,20,53,226,37,133,215,119,171,130,96,107,239,245,109,145,250}
+	if shared.EGGHUNTCHATTINGCONNECTION then
+		pcall(function() shared.EGGHUNTCHATTINGCONNECTION:Disconnect() end)
+	end
+	shared.EGGHUNTCHATTINGCONNECTION = lplr.Chatted:Connect(function(msg)
+		if (msg:split(" "))[1] == "/eggclaim" then
+			game:GetService("ReplicatedStorage").rbxts_include.node_modules["@rbxts"].net.out._NetManaged.EggHunt2025_CheatcodeActivatedFromClient:FireServer({
+				hash = cheat
+			})
+			game:GetService('StarterGui'):SetCore('SendNotification', {
+				Title = 'Voidware',
+				Text = 'Successfully claimed the Cheatcode Egg!',
+				Duration = 10,
+			})
+			pcall(function() shared.EGGHUNTCHATTINGCONNECTION:Disconnect() end)
+		end
+	end)
 end)
 
 run(function()
