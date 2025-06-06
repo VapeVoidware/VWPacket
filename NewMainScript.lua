c1eaf9429354fe9c7afc1c735976dbcf4ffa4520
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-06-06 17:49:39 +0300

    Nuker & BedAssist Upgrades! 🚀

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -210,7 +210,7 @@ if not are_installed_1() then install_profiles(1) end
 local url = 'https://github.com/VapeVoidware/VWPacket'
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = '07207da6f4060693e005f36dde2ad17c8b2ba1a0'
+commit = '21e96626e6d6aea6a3a126ff98877bb072e7d1c9'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
