b663b0d0d7d1413e46236dcb2d3a3b9594ac20af
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-07-07 22:15:10 +0300

    Invisibility, PlayerAttach & Damage Indicator! 🚀

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -210,7 +210,7 @@ if not are_installed_1() then install_profiles(1) end
 local url = 'https://github.com/VapeVoidware/VWPacket'
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = '4b0a1796675ce3d1742aec33ca4c44893e887b1a'
+commit = 'd603fbde8150b7fb829a96f9d6f7d24204c29912'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
