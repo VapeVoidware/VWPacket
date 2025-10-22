dda3e2784d10f1e099c9354b2f80c8d19c408632
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-10-22 18:26:02 +0300

    Update CE6872274481.lua

--- games/CE6872274481.lua
+++ games/CE6872274481.lua
@@ -4195,8 +4195,8 @@ local function isFirstPerson()
 	return (entitylib.character.Head.Position - gameCamera.CFrame.Position).Magnitude < 2
 end
 
-bedwars.ViewModel = workspace.CurrentCamera.Viewmodel.RightHand.RightWrist
-local oldrotation = bedwars.ViewModel.C0
+--bedwars.ViewModel = workspace.CurrentCamera.Viewmodel.RightHand.RightWrist
+--local oldrotation = bedwars.ViewModel.C0
 
 local originalArmC0, originalNeckC0, originalRootC0
 
